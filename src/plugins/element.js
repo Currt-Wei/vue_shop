24ccf62830c3d8fd8cedd148864dee9980aed09c
Currt-Wei <2598230239@qq.com>
2020-07-19 10:10:42 +0800

    登陆功能完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
-
+import { Form, FormItem} from 'element-ui'
+import { Input } from 'element-ui'
+import { Message } from 'element-ui'
+// 注册为全局可用
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message=Message;
